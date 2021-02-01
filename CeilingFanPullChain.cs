d7f53ca80e1f94e5efe0ac8776f3761b80040a3c
WangPinChieh <pp8101313@hotmail.com>
2021-02-01 09:22:19 +0800

    replace if-statement with switch-statement, and extract method SetState

--- CeilingFanPullChain.cs
+++ CeilingFanPullChain.cs
@@ -13,26 +13,30 @@ namespace Refactoring_StatePattern
 
         public void pull()
         {
-            if (currentState == 0)
+            switch (currentState)
             {
-                currentState = 1;
-                Console.WriteLine("low speed");
+                case 0:
+                    SetState(1);
+                    Console.WriteLine("low speed");
+                    break;
+                case 1:
+                    SetState(2);
+                    Console.WriteLine("medium speed");
+                    break;
+                case 2:
+                    SetState(3);
+                    Console.WriteLine("high speed");
+                    break;
+                default:
+                    SetState(0);
+                    Console.WriteLine("turning off");
+                    break;
             }
-            else if (currentState == 1)
-            {
-                currentState = 2;
-                Console.WriteLine("medium speed");
-            }
-            else if (currentState == 2)
-            {
-                currentState = 3;
-                Console.WriteLine("high speed");
-            }
-            else
-            {
-                currentState = 0;
-                Console.WriteLine("turning off");
-            }
+        }
+
+        private void SetState(int state)
+        {
+            currentState = state;
         }
     }
 }
