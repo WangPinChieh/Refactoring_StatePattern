639505abfab0a95f2033c3b1064d78f83d363125
WangPinChieh <pp8101313@hotmail.com>
2021-02-01 08:39:58 +0800

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Refactoring_StatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
